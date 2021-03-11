a16fcdf9fb933e85a4ec7632458e204f0372dd19
miguelbravo7 <miguelbravo7@gmail.com>
2021-03-11 18:16:22 +0000

    Removed optional chaining.

--- src/AudioSpectrum.js
+++ src/AudioSpectrum.js
@@ -1,10 +1,11 @@
 const EventEmitter = require('events')
 const AudioAnalyser = require('./AudioAnalyser.js')
+
 module.exports = class AudioSpectrum {
   constructor (visualizer) {
     this.analyser = null
     this.visualizer = visualizer
-    this.visualizer.subscribe(function (ctx) { this.analyser?.updateFftSize(ctx.fftSize) }.bind(this))
+    this.visualizer.subscribe(function (ctx) { this.analyser.updateFftSize(ctx.fftSize) }.bind(this))
     this.eventEmitter = new EventEmitter()
     this.eventEmitter.on('draw', () => {
       window.requestAnimationFrame(this.draw.bind(this))
@@ -12,8 +13,8 @@ module.exports = class AudioSpectrum {
     window.requestAnimationFrame(this.draw.bind(this))
   }
 
-  get canvas () { return this.visualizer?.canvas }
-  get isReady () { return this.analyser?.isReady && this.visualizer != null }
+  get canvas () { return this.visualizer.canvas }
+  get isReady () { return this.analyser && this.analyser.isReady && this.visualizer != null }
   get playback () { return this.analyser.howl.seek() / this.analyser.howl.duration() }
   set playback (position) {
     this.analyser.howl.seek(position * this.analyser.howl.duration())
@@ -21,11 +22,11 @@ module.exports = class AudioSpectrum {
   }
 
   setAudio (howl, ctx) { this.analyser = new AudioAnalyser(howl, this.visualizer.fftSize, ctx) }
-  destroy () { this.analyser?.destroy() }
+  destroy () { this.analyser && this.analyser.destroy() }
 
   draw (timestamp) {
-    if (this.analyser?.isReady) {
-      this.visualizer?.draw(this.analyser.spectrumArray, timestamp, this.playback)
+    if (this.analyser && this.analyser.isReady) {
+      this.visualizer.draw(this.analyser.spectrumArray, timestamp, this.playback)
     }
     this.eventEmitter.emit('draw')
   }
